016245e0c5a38536aeb88b48ed0235dcb5b9b11a
shuvo9620 <smahfuzur0@gmail.com>
2023-06-02 15:08:04 +0600

    card added and data shows

--- src/components/ShowList.jsx
+++ src/components/ShowList.jsx
@@ -1,8 +1,28 @@
+import { useState, useEffect } from 'react';
+import ShowSummary from './ShowSummary';
 
 const ShowList = () => {
+    const [shows, setShows] = useState([]);
+
+    const url = 'https://api.tvmaze.com/search/shows?q=all';
+
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setShows(data))
+            .catch(error => {
+                console.error('Error fetching shows:', error);
+            });
+    }, []);
+
     return (
-        <div>
-            <h2>this is show list</h2>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-8'>
+            {
+                shows.map(show => <ShowSummary
+                    key={show.id}
+                    show={show}
+                ></ShowSummary>)
+            }
         </div>
     );
 };
--- src/components/ShowSummary.jsx
+++ src/components/ShowSummary.jsx
@@ -1,8 +1,17 @@
 
-const ShowSummary = () => {
+const ShowSummary = ({ show }) => {
     return (
         <div>
-            <h2>this is summary</h2>
+            <div className="card card-side bg-base-100 shadow-xl">
+                <figure><img src={show.show.image.medium} alt="Movie" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">New movie is released!</h2>
+                    <p>Click the button to watch on Jetflix app.</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Watch</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
